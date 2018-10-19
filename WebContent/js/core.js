861c2f38a837afee34ff89c0f1744681bb9a1751
Sannita <luca.lupacchino@gmail.com>
2018-10-19 15:20:26 +0200

    added focus on container added snake game

--- WebContent/js/core.js
+++ WebContent/js/core.js
@@ -1,7 +1,7 @@
 window.gameJs = window.gameJs || {};
 
 (function(g, undefined){
-	
+
 	var init = function(){
 		var vendors = ['webkit', 'moz'];
 		for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
@@ -25,12 +25,12 @@ window.gameJs = window.gameJs || {};
 				clearTimeout(id);
 			};
 		}
-	
+
 	}
 
-	
+
 	var Core = function(){
-		
+
 		this.config = {
 			width : 0,
 			height : 0,
@@ -39,16 +39,16 @@ window.gameJs = window.gameJs || {};
 			tps : 25,
 			maxFrameSkip : 5
 		};
-		
+
 		this.ctx = null;
 		this.canvas = null;
 		this.container = null;
 		this.input = null;
 		this.items = null;
-		
+
 		init();
 	}
-	
+
 	Core.prototype.handleInput = function(){
 		for (var i in this.items) {
 			if (this.items.hasOwnProperty(i)) {
@@ -63,7 +63,7 @@ window.gameJs = window.gameJs || {};
 			}
 		}
 	}
-	
+
 	Core.prototype.update = function(t){
 		for (var i in this.items) {
 			if (this.items.hasOwnProperty(i)) {
@@ -78,7 +78,7 @@ window.gameJs = window.gameJs || {};
 			}
 		}
 	}
-	
+
 	Core.prototype.render = function(alpha){
 		this.ctx.clearRect(0,0,this.config.width,this.config.height);
 		for (var i in this.items) {
@@ -94,19 +94,19 @@ window.gameJs = window.gameJs || {};
 			}
 		}
 	}
-	
+
 	Core.prototype.log = function(item){
 		if(this.config.debug){
 			console.log(item);
 		}
 	}
-	
+
 	Core.prototype.setup = function(containerId, width, height){
-		
+
 		this.container = document.getElementById(containerId);
 		this.config.width = width;
 		this.config.height = height;
-		
+
 		this.canvas = document.createElement('canvas');
 		this.canvas.width = width;
 		this.canvas.height = height;
@@ -114,26 +114,26 @@ window.gameJs = window.gameJs || {};
 		if (typeof (this.canvas.getContext) === undefined) {
 			throw new Error('not canvas');
 		}
-		
+
 		this.container.appendChild(this.canvas);
 		this.container.style.width = this.canvas.width + 'px';
 		this.container.style.height = this.canvas.height + 'px';
-
+		
 		this.ctx = this.canvas.getContext('2d');
 		this.items = {};
 	}
-	
+
 	Core.prototype.start = function(){
 		var self = this;
 		this.config.running = true;
-		
+
 		var dt = 1000 / this.config.tps;
 		var currentTime = performance.now();
 		var accumulator = 0;
 		var t = 0;
-		
+
 		var loop = function(timestamp) {
-		
+
 			self.handleInput();
 
 			var newTime =  performance.now();
@@ -157,23 +157,23 @@ window.gameJs = window.gameJs || {};
 				window.requestAnimationFrame(loop);
 			}
 		}
-		
+
 		loop(performance.now());
 
 	}
-	
+
 	Core.prototype.stop = function(){
 		this.config.running = false;
 	}
-	
+
 	Core.prototype.addListener = function(name, callback){
 		this.container.addEventListener(name, callback, false);
 	}
-	
+
 	Core.prototype.initListeners = function() {
 		var self = this;
 		self.input = {};
-		
+
 		this.addListener('keydown', function(key){
 		    self.input[key.code] = true;
 			self.log(key);
@@ -183,7 +183,7 @@ window.gameJs = window.gameJs || {};
 			self.input[key.code] = false;
 			self.log(key);
 		}, false);
-		
+
 		/*container_.addEventListener('click', function(evt){
 			input_['click'] = true;
 			input_['mouseX'] = evt.clientX;
@@ -199,7 +199,7 @@ window.gameJs = window.gameJs || {};
 			this.input['mouseY'] = evt.clientY;
 			this.log(evt);
 		}, false);
-		
+
 		this.addListener('mouseup', function(evt){
 			this.input['click'] = false;
 			this.input['mouseup'] = true;
@@ -208,28 +208,28 @@ window.gameJs = window.gameJs || {};
 			this.input['mouseY'] = evt.clientY;
 			this.log(evt);
 		}, false);
-		
+
 		this.addListener('mousemove', function(evt){
 			this.input['mouseX'] = evt.clientX;
 			this.input['mouseY'] = evt.clientY;
 			this.log(evt);
 		}, false);
 */
-
+		this.container.focus();
 	}
-	
+
 	Core.prototype.setDebug = function(debug){
 		this.config.debug = debug;
 	}
-	
+
 	Core.prototype.addItem = function(item){
 		this.items[item.id] = item;
 	}
-	
+
 	Core.prototype.deleteItem = function(item){
 		delete this.items[item.id];
 	}
-	
+
 	g.Core = new Core();
-	
+
 })(window.gameJs);
