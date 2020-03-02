40b52b7a3350997b5e735a07d052217bd7809f44
Luca LUPACCHINO <luca.lupacchino@alten.it>
2020-03-02 16:10:32 +0100

    fix

--- modules/engine/core.js
+++ modules/engine/core.js
@@ -1,28 +1,36 @@
 export { Core }
 
-let vendors = ['webkit', 'moz']
-for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
-    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame']
-}
-
-let lastTime = 0
-if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function (callback, element) {
-        let currTime = performance.now()
-        let timeToCall = Math.max(0, 16 - (currTime - lastTime))
-        let id = setTimeout(() => {
-            callback(currTime + timeToCall)
-        }, timeToCall)
-        lastTime = currTime + timeToCall
-        return id
+let initialize = () => {
+    if(initialized){
+        return;
     }
-}
 
-if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function (id) {
-        clearTimeout(id)
+    let vendors = ['webkit', 'moz']
+    for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame']
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame']
     }
+
+    let lastTime = 0
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            let currTime = performance.now()
+            let timeToCall = Math.max(0, 16 - (currTime - lastTime))
+            let id = setTimeout(() => {
+                callback(currTime + timeToCall)
+            }, timeToCall)
+            lastTime = currTime + timeToCall
+            return id
+        }
+    }
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id)
+        }
+    }
+
+    var initialized = true
 }
 
 class Core {
--- modules/plague.js
+++ modules/plague.js
@@ -10,11 +10,25 @@ let launch = () => {
     core.start()
 
     let p1 = new Point(20, 10)
-    let p2 = new Point(50, 30)
-    let line = new Line(p1, p2)
-    let renderer = new LineRenderer()
-    let item = new Item(line, renderer)
-
-    core.addItem(item)
+    let p2 = new Point(620, 10)
+    let p3 = new Point(20, 350)
+    let p4 = new Point(620, 350)
+    let line1 = new Line(p1, p2)
+    let line2 = new Line(p2, p3)
+    let line3 = new Line(p3, p4)
+    let line4 = new Line(p4, p1)
+    let renderer1 = new LineRenderer()
+    let renderer2 = new LineRenderer({ lineWidth : '10'})
+    let renderer3 = new LineRenderer({ lineCap : 'butt', lineJoin : 'square', lineWidth : '1', strokeStyle : 'blue'})
+    let renderer4 = new LineRenderer({ lineCap : 'square', lineJoin : 'circle', lineWidth : '3', strokeStyle : 'green'})
+    let item1 = new Item(line1, renderer1)
+    let item2 = new Item(line2, renderer2)
+    let item3 = new Item(line3, renderer3)
+    let item4 = new Item(line4, renderer4)
+    
+    core.addItem(item1)
+    core.addItem(item2)
+    core.addItem(item3)
+    core.addItem(item4)
 
 }
