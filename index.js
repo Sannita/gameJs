2ac4e3791a58674579e35d770cf0781f91b3773e
Luca LUPACCHINO <luca.lupacchino@alten.it>
2020-03-02 13:11:54 +0100

    git ignore

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let buttonListener = (event) => {
+    let container = document.querySelector('#container')
+    
+    while(container.firstChild){
+        container.removeChild(container.lastChild)
+    }
+
+    container.style.width = '0px'
+    container.style.height = '0px'
+    
+    import('./modules/'+app+'.js').then(module=>{
+        module.default('#container', 640, 360)
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+let initButton = (app) => {
+    let button = document.createElement('button')
+    let text = document.createTextNode(app)
+    button.appendChild(text)
+    button.addEventListener('click', buttonListener)
+
+    let buttonsContainer = document.querySelector('#buttons')
+    buttonsContainer.appendChild(button)
+}
+
 let apps = [
     'plague'
     , 'maze' 
@@ -6,30 +33,4 @@ let apps = [
     //,'balls'
     //,'bezier'
 ]
-
-let buttonsContainer = document.querySelector('#buttons')
-    
-apps.forEach(app => {
-    let button = document.createElement('button')
-    
-    button.addEventListener('click', event => {
-        let container = document.querySelector('#container')
-        while(container.firstChild){
-            container.removeChild(container.lastChild)
-        }
-
-        container.style.width = '0px'
-        container.style.height = '0px'
-        
-        import('./modules/'+app+'.js').then(module=>{
-            module.default('#container', 640, 360)
-        }).catch(err => {
-            console.log(err)
-        })
-    })
-
-    let text = document.createTextNode(app)
-    button.appendChild(text)
-
-    buttonsContainer.appendChild(button)
-})
+apps.forEach(app => initButton)
